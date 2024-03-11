f4d714a7c9970ada502bf33c0de897f99b02b0d8
yusuf sonmez <yusufsonmez951@gmail.com>
2024-03-11 15:01:39 +0300

    Initial commit

---
+++ src/main/java/bigbox/example/ecommenceProject/EcommenceProjectApplication.java
@@ -0,0 +1,13 @@
+package bigbox.example.ecommenceProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommenceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommenceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bigbox/example/ecommenceProject/EcommenceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package bigbox.example.ecommenceProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommenceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
